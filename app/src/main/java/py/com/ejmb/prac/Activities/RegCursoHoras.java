669a87c2a84cac4f4fe40e1373354e70c151264f
Eduardo <edum87@gmail.com>
2016-10-08 00:46:01 -0300

    RegCursoHoras

---
+++ app/src/main/java/py/com/ejmb/prac/Activities/RegCursoHoras.java
@@ -0,0 +1,15 @@
+package py.com.ejmb.prac.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import py.com.ejmb.prac.R;
+
+public class RegCursoHoras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_curso_horas);
+    }
+}
